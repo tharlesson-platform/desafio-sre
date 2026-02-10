9df22ee2aec1661a080dd8dca32bce704ec1208d
Tharlesson Souza <tharlesson.souza@tqi.com.br>
2026-02-10 08:58:05 -0300

    Update versions

--- iac/versions.tf
+++ iac/versions.tf
@@ -4,11 +4,11 @@ terraform {
   required_providers {
     oci = {
       source  = "oracle/oci"
-      version = "~> 5.0"
+      version = "8.0.0"
     }
     random = {
       source  = "hashicorp/random"
-      version = "~> 3.6"
+      version = "3.7.2"
     }
   }
 }
